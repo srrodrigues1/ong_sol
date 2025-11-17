cf7f8b77f6203390d2c58ae00cd9c267e514bfa0
SRR <samurodrigues.11@gmail.com>
2025-11-17 13:17:03 -0300

    Corrigindo inicialização dos containers

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,6 +8,12 @@ python manage.py makemigrations --noinput
 echo "Aplicando migrações..."
 python manage.py migrate --noinput
 
+echo "Carregando fixtures..."
+python manage.py loaddata equipment_types.json
+python manage.py loaddata roles.json
+python manage.py loaddata admin_user.json
+
+
 echo "Coletando arquivos estáticos..."
 python manage.py collectstatic --noinput
 
